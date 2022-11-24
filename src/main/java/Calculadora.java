f5d5d97c962e67c75dd035547d7237011b342bec
lopezrichard <84924555+lopezrichard@users.noreply.github.com>
2022-11-24 12:34:51 -0400

    Update Calculadora.java

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -5,7 +5,7 @@
 public class Calculadora {
     public static int suma (int a,int b){
         return a +b;
-        ///
+        //////
     }
     public static int resta (int a,int b){
         return a -b;
