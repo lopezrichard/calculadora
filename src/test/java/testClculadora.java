5d5715477ea23bd6d1843be2d531833e0b0e986c
Richard <global>
2022-11-24 14:33:49 -0400

    commit

--- src/test/java/testClculadora.java
+++ src/test/java/testClculadora.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by Richard Lopez on 24/11/2022
@@ -9,10 +9,10 @@ public class testClculadora {
     @Test
     public void suma(){
         //
-        Assertions.assertEquals(Calculadora.suma(5,5),10,"La suma no es correcta");
+        Assert.assertEquals(Calculadora.suma(5,5),10,"La suma no es correcta");
     }
     @Test
     public void resta(){
-        Assertions.assertEquals(Calculadora.resta(5,5),0,"La resta no es correcta");
+        Assert.assertEquals(Calculadora.resta(5,5),0,"La resta no es correcta");
     }
 }
