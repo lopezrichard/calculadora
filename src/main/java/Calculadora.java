981bb9efcea1d92af9dae5166f8534187c63d0de
Richard <global>
2022-11-24 09:53:49 -0400

    Initial commit

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Richard Lopez on 24/11/2022
+ * Visit Linkedin https://www.linkedin.com/in/richard-lopez-/
+ **/
+public class Calculadora {
+    public static int suma (int a,int b){
+        return a +b;
+    }
+    public static int resta (int a,int b){
+        return a -b;
+    }
+
+}
---
+++ src/test/java/testClculadora.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Richard Lopez on 24/11/2022
+ * Visit Linkedin https://www.linkedin.com/in/richard-lopez-/
+ **/
+public class testClculadora {
+    @Test
+    public void suma(){
+        Assertions.assertEquals(Calculadora.suma(5,5),10,"La suma no es correcta");
+    }
+    @Test
+    public void resta(){
+        Assertions.assertEquals(Calculadora.resta(5,5),0,"La resta no es correcta");
+    }
+}
